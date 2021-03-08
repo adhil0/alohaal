2dfdc13ada52f601fe32af354f6660131d47123c
adhil0 <adhitya@ucsb.edu>
2021-03-08 01:16:20 -0800

    finished pika data

--- data-raw/and_vertebrates.R
+++ data-raw/and_vertebrates.R
@@ -11,7 +11,31 @@ and_download <- download_d1_data(data_url = and_url, path = tempdir())
 
 # Read in data
 and_files <- read_d1_files(and_download)
-and_vertebrates <- and_files$data
+and_vertebrates_raw <- and_files$data
+and_vertebrates <- and_vertebrates_raw %>%
+  select(YEAR,
+         SITECODE,
+         SECTION,
+         REACH,
+         SPECIES,
+         LENGTH1,
+         LENGTH2,
+         WEIGHT,
+         SAMPLEDATE) %>%
+  mutate(
+    SPECIES = recode(
+      SPECIES,
+      "RHOL" = "Olympic Torrent Salamander",
+      "ONCL" = "Coastal Cutthroat Trout",
+      "DITE" = "Coastal Giant Salamander"
+    ),
+    SITECODE = as.factor(SITECODE),
+    SECTION = as.factor(SECTION),
+    REACH = as.factor(REACH)
+  )
+
+#select _>mutate <- recode
+  #drop SELECT Year sitecode section reach species length1 length2 weight sampledate
 
 # Save the data as a data object (.Rda) with usethis::use_data() - this code should already exist in the script, just update the dataset name
 # NOTE: You could do some wrangling HERE to simplify the dataset before storing the .Rda, but we’ll just use it as read in from metajam today
