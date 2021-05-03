3d7d75c5eb002971cd4c8a532489d6ee9a2f505d
Adhitya Logan <adhityalogan@gmail.com>
2021-05-02 17:20:54 -0700

    added ntl ice and temps docs, data, and vignettes. Couldn't run ntl_temps data raw though.

--- R/ntl_ice_doc.R
+++ R/ntl_ice_doc.R
@@ -1,10 +1,19 @@
-
-#' North Temperate Lakes LTER: Ice Duration - Madison Lakes Area 1853 - current
+#' North Temperate Lakes LTER: Ice Duration - Madison Lakes Area 1853 - current & Madison Wisconsin Daily Meteorological Data 1869 - current
 #'
-#' Data includes lake name, dates of freeze-up and thaw, and duration of ice
-#' cover of lakes in the Madison, WI area.
+#' Data includes lake name, dates of freeze-up and thaw, and duration of ice cover of lakes in the Madison, WI area. Ice cover duration is the number of days that a lake is frozen, excluding periods where the lake thaws before refreezing again. Lakes Monona and Wingra are considered to be frozen if they are completely ice covered,
+#' while Lake Mendota is considered to be frozen if there is ice from Picnic Point to Maple Bluff and more than 50% of the lake is covered by ice.
 #'
-#' @source {Magnuson, J.J., S.R. Carpenter, and E.H. Stanley. 2021. North Temperate Lakes LTER: Ice Duration - Madison Lakes Area 1853 - current ver 35. Environmental Data Initiative. https://doi.org/10.6073/pasta/ab31f2489ee436beb73fc8f1d0213d97 (Accessed 2021-03-01).}
-#' \url{https://portal.edirepository.org/nis/mapbrowse?scope=knb-lter-ntl&identifier=33&revision=35}
-
+#' @format A tibble with 431 rows and 5 variables
+#' \describe{
+#'   \item{lakeid}{a factor denoting the lake name}
+#'   \item{ice_on}{a Date denoting the freeze date of each lake}
+#'   \item{ice_off}{a Date denoting the breakup date of each lake}
+#'   \item{ice_duration}{a number denoting the number of days between the freeze and breakup dates of each lake}
+#'   \item{year}{a number denoting the year of observation}
+#'   }
+#' @source {Magnuson, J.J., S.R. Carpenter, and E.H. Stanley. 2021. North Temperate Lakes LTER: Ice Duration - Madison Lakes Area 1853 - current ver 35. Environmental Data Initiative.}
+#' \url{https://doi.org/10.6073/pasta/ab31f2489ee436beb73fc8f1d0213d97}
 "ntl_ice"
+
+
+
